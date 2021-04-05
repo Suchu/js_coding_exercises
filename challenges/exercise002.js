97816ae08480a5e8173b5d75dfb94d0c196276b5
Sulochana Bhujel <sulochana.bhujel92@gmail.com>
2021-04-05 10:24:32 +0100

    Implemented exercise002 - countSheep

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -20,7 +20,16 @@ function getBusNumbers(people) {
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  let count = 0;
+  for ( let i = 0; i < arr.length; i++) {
+    if(arr[i] === "sheep") {
+      count++
+    }
+    else {
+      count;
+    }
+  }
+  return count;
 }
 
 function hasMPostCode(person) {
