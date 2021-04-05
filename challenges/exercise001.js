4f8f050919143cddc7c5a7acede9ab21b609ba4d
Sulochana Bhujel <sulochana.bhujel92@gmail.com>
2021-04-05 08:29:08 +0100

    Implemented exercise001 - getSalePrice

--- challenges/exercise001.js
+++ challenges/exercise001.js
@@ -18,7 +18,7 @@ function addVAT(originalPrice, vatRate) {
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-  // Add your code here!
+  return parseFloat((originalPrice * (1 - reduction/100)).toFixed(2).replace(/[.,]00$/, ""));
 }
 
 function getMiddleCharacter(str) {
