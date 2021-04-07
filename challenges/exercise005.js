dc1138dd48bfbf104809243e261f999a8f85a009
Sulochana Bhujel <sulochana.bhujel92@gmail.com>
2021-04-07 12:58:53 +0100

    Implemented exercise005 - reverseNumber

--- challenges/exercise005.js
+++ challenges/exercise005.js
@@ -24,7 +24,8 @@ const count1sand0s = str => {
 
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+  
+  return Number(String(n).split("").reverse().join(""));
 };
 
 const sumArrays = arrs => {
