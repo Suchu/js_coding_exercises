03e0f1ae1593a95a9ca0273cf43f562fea342820
Sulochana Bhujel <sulochana.bhujel92@gmail.com>
2021-04-08 18:02:01 +0100

    Implemented exercise006 - isValidDNA

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -22,6 +22,7 @@ const sumMultiples = arr => {
  */
 const isValidDNA = str => {
   if (str === undefined) throw new Error("str is required");
+  return str.toUpperCase().match(/^[GTAC>]*$/) ? true : false;
 };
 
 /**
--- test/exercise006.test.js
+++ test/exercise006.test.js
@@ -1,4 +1,4 @@
-const { sumMultiples } = require("../challenges/exercise006");
+const { sumMultiples, isValidDNA } = require("../challenges/exercise006");
 
 describe("sumMultiples", () => {
     test("returns the sum of any numbers which are a multiple of 3 or 5", () => {
@@ -11,5 +11,23 @@ describe("sumMultiples", () => {
 
     test("returns 0 if the sum of any numbers which are a multiples of 3 or 5 is negative ", () => {
         expect(sumMultiples([2, -3, 7, -5])).toBe(0);
+    });
+});
+
+describe("isValidDNA", () => {
+    test("returns true if string of characters contain characters C, G, T or A only", () => {
+        expect(isValidDNA("gtacttacg")).toBe(true);
+    });
+
+    test("returns false if string of characters doesn't contain characters C, G, T or A", () => {
+        expect(isValidDNA("MBBS")).toBe(false);
+    });
+
+    test("returns false if string of characters contain characters C, G, T or A with other character", () => {
+        expect(isValidDNA("MBBSGTAC")).toBe(false);
+    });
+
+    test("Ignore lowercase", () => {
+        expect(isValidDNA("gaaccgtt")).toBe(true);
     });
 });
